5e806c4ab6e25baf3bbec4eb63c2293bf8ba0520
Latha <lathanavaneeth@gmail.com>
2019-10-24 09:51:07 +1100

    first commit

---
+++ Readme.md
@@ -0,0 +1,33 @@
+Music Festival  for EnergyAustralia Coding Test
+This is simple program in respond to EnergyAustralia coding test, which request data from swagger API and then parse into a particular manner: at the top level, it should show the band record label, below that it should list out all bands under their management, and below that it should display which festivals they've attended, if any. All entries should be sorted alphabetically.
+
+For example:
+
+Record Label 1
+  Band X
+    Omega Festival
+  Band Y
+Record Label 2
+  Band A
+    Alpha Festival
+    Beta Festival
+
+Technologies used
+Ruby
+
+Gems used:
+pry
+sinatra
+sinatra/reloader
+httparty
+
+
+Solved issue
+
+1. swagger API sometime return response code 429 and cause the program exit in error
+2. response data from swagger API sometime is missing few chunk of data which may crashing my site
+
+Have worked out how to better handle errors caused the API responding with an empty array or with a 429 code. Individual messages are now being displayed.
+
+Author
+Latha Krishna 
---
+++ main.rb
@@ -0,0 +1,94 @@
+require 'pry'
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'httparty'
+
+get '/' do
+  url = "http://eacodingtest.digital.energyaustralia.com.au/api/v1/festivals"
+  result = HTTParty.get(url)
+
+  # using this to log in terminal the result.code trying to sort out throttling
+  case result.code
+  when 200
+    puts "All good!"
+  when 429
+    puts "Throttled"
+    @result = "Too many requests, throttling"
+    erb :index
+  end
+
+  if result[0] == nil 
+    @result = 'No information'
+    erb :index
+  end  
+  
+  record_labels = []
+  bands = []
+  music_festival = []
+
+  for i in result do
+
+    if i['name'] == false
+      puts 'not working'
+    end 
+
+    if i['name']
+        music_festival << i['name']
+    end
+
+    if i['bands']
+      i['bands'].each do |band| 
+        bands << band['name']
+        if band['recordLabel'] != '' && band['recordLabel'] != nil
+          record_labels << band['recordLabel'] 
+        end
+      end
+    end
+
+    if i['bands'] == false
+      @record_labels_hash = 'No information'
+      erb :index
+    end
+
+  end
+
+  record_labels_sorted_array = record_labels.uniq.sort
+  unique_bands = bands.uniq.sort
+
+  @record_labels_hash = {
+
+  }
+
+  record_labels_sorted_array.each do |record_label| 
+    @record_labels_hash[record_label] = {}
+
+    for i in result do 
+      i['bands'].each do |band| 
+        if band['recordLabel'] != '' && band['recordLabel'] != nil && band['recordLabel'] == record_label 
+          @record_labels_hash[record_label][band['name']] = []
+        end
+      end
+    end
+
+
+    unique_bands.each do |unique_band|
+      for i in result do
+        i['bands'].each do |band| 
+          if band['name'] == unique_band && @record_labels_hash[record_label][band['name']] != nil
+            if i['name'] != nil 
+              @record_labels_hash[record_label][band['name']] << i['name']
+            end   
+          end
+        end
+      end
+    end
+  end
+
+
+  @record_labels_hash.each do | key, value |
+      @record_labels_hash[key] = @record_labels_hash[key].sort.to_h
+  end
+  
+  erb :index
+
+end
